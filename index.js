ad063f88cc44d96d68df45c3ee3a128b152b68fb
Jeff Andersen <jeffandersen@gmail.com>
2014-11-14 21:18:26 -0400

    Add hockey schedule app

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 
 var errors = require('./lib/errors');
-var session = require('./lib/session');
 var routes = require('./lib/routes/index');
-var auth = require('./lib/routes/auth');
 
 var app = express();
 
@@ -22,12 +20,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// sessions and authentication
-session.initialize(app);
-
 // routing
 app.use('/', routes);
-app.use('/auth', auth);
 
 // error handling
 errors.notfound(app);
